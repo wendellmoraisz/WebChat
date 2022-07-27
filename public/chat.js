7cb797c315e8c29b0d1d93d012794d8899ece588
Wendell <wendellmorais3319@gmail.com>
2022-07-27 10:07:48 -0300

    feat: nome do usuário na mensagem

--- public/chat.js
+++ public/chat.js
@@ -3,17 +3,26 @@ const socket = io();
 const form = document.getElementById("form");
 const input = document.getElementById("inputMessage");
 const messages = document.querySelector(".messages");
+const urlSearch = new URLSearchParams(window.location.search);
+const username = urlSearch.get("nickname");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
     if (input.value) {
-        socket.emit("chat message", input.value);
+        socket.emit("chat message", { username: username, content: input.value });
         input.value = "";
     }
 });
 
 socket.on("chat message", msg => {
     const item = document.createElement("p");
-    item.textContent = msg;
+    const userNameSpan = document.createElement("span");
+
+    userNameSpan.classList.add("username");
+    userNameSpan.textContent = msg.username;
+
+    item.appendChild(userNameSpan);
+    item.innerHTML += msg.content;
+
     messages.appendChild(item);
 });
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,1 +1,11 @@
 import { io } from "./http";
+interface UserMessage {
+    username: string
+    content: string
+}
+
+io.on("connection", socket => {
+    socket.on("chat message", (msg: UserMessage) => {
+        io.emit("chat message", msg);
+    });
+});
